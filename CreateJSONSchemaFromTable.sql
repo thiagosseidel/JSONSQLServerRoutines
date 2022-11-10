bf0afd6aa522c4bf7f2b23cbd3ef75aca49e56dc
Thiago Soares Seidel <thiagosseidel@unisinos.br>
2022-11-10 00:23:51 -0300

    Evolução: array

--- CreateJSONSchemaFromTable.sql
+++ CreateJSONSchemaFromTable.sql
@@ -35,38 +35,63 @@ DECLARE @required NVARCHAR(max), @NoColumns INT, @properties NVARCHAR(max);
            
            DECLARE @SourceCode NVARCHAR(255)=
            (SELECT 'SELECT * FROM '+QuoteName(@database)+ '.'+ QuoteName(@Schema)+'.'+QuoteName(@table))
-           SELECT 
+            SELECT 
              @properties= String_Agg(
-             (CASE WHEN one_of IS NULL THEN 
-               	'"'+f.name+'": {"type":["'+Replace(type,' ','","')+'"],'+
-               	(CASE WHEN format IS NOT NULL THEN '"format":"'+format+'",' ELSE '' END)+
-               	'"sqltype":"'+sqltype+'", "columnNo":'+ Convert(VARCHAR(3), f.column_ordinal)
-           		+', "nullable":'+Convert(CHAR(1),f.is_nullable)+', "Description":"'
-               	+String_Escape(Coalesce(Convert(NvARCHAR(875),EP.value),''),'json')+'"}' ELSE
-				'"'+f.one_of+'": {"oneOf":[{"$ref": "#/definitions/'+f.one_of+'"}]'+'}'
-				END),','),
+	             	(CASE WHEN array_type IS NOT NULL THEN
+	             		'"'+f.name+
+	             		'": {"type":["'+f.type+'"],'+
+	             		'"items":{"$ref": "#/definitions/'+f.array_type+'"}'+'}'
+		             WHEN one_of IS NULL THEN 
+	               		'"'+f.name+'": {"type":["'+Replace(type,' ','","')+'"],'+
+	               		(CASE WHEN format IS NOT NULL THEN '"format":"'+format+'",' ELSE '' END)+
+	               		'"sqltype":"'+sqltype+'", "columnNo":'+ Convert(VARCHAR(3), f.column_ordinal)
+	           			+', "nullable":'+Convert(CHAR(1),f.is_nullable)+', "Description":"'
+	               		+String_Escape(Coalesce(Convert(NvARCHAR(875),EP.value),''),'json')+'"}'
+					ELSE
+						'"'+f.one_of+'": {"oneOf":[{"$ref": "#/definitions/'+f.one_of+'"}]'+'}'
+					END
+					), ','),
              @NoColumns=Max(f.column_ordinal),
              @required=String_Agg((CASE WHEN is_nullable = 0 THEN '"'+f.Name+'"' ELSE '""' END), ',')
              FROM
                ( --the basic columns we need. (the type is used more than once in the outer query) 
-               SELECT 
-           	  	 r.name, 
-           	  	 CASE WHEN r.system_type_id IN (42, 58, 61) THEN 'date-time'
+	               SELECT *
+					FROM
+					(SELECT 
+					 r0.name, 
+					 CASE WHEN r0.system_type_id IN (42, 58, 61) THEN 'date-time'
 					WHEN system_type_id = 40 THEN 'date'
 					WHEN system_type_id = 41 THEN 'time' ELSE NULL END AS format,
-                 r.system_type_name  AS sqltype,
-                 r.source_column,
-                 r.is_nullable,r.column_ordinal,
-                 CASE WHEN r.system_type_id IN (48, 52, 56) THEN 'integer'
-           	     	WHEN r.system_type_id IN (59, 60, 62, 106, 108, 122, 127) THEN 'number'
-                   WHEN system_type_id = 104 THEN 'boolean' ELSE 'string' END
-                 + CASE WHEN r.is_nullable = 1 THEN ' null' ELSE '' END AS type,
-                 (select object_name(fkc.referenced_object_id) from sys.foreign_key_columns fkc
-					where fkc.parent_column_id = r.column_ordinal AND
-					fkc.parent_object_id = CAST(Object_Id(r.source_database + '.' + r.source_schema + '.' + r.source_table) AS INT)) AS one_of,
-                 Object_Id(r.source_database + '.' + r.source_schema + '.' + r.source_table) AS table_id
-                 FROM sys.dm_exec_describe_first_result_set
-           	    (@sourcecode, NULL, 1) AS r
+					 r0.system_type_name AS sqltype,
+					 r0.source_column,
+					 r0.is_nullable,
+					 r0.column_ordinal,
+					 CASE WHEN r0.system_type_id IN (48, 52, 56) THEN 'integer'
+					WHEN r0.system_type_id IN (59, 60, 62, 106, 108, 122, 127) THEN 'number'
+					   WHEN system_type_id = 104 THEN 'boolean' ELSE 'string' END
+					 + CASE WHEN r0.is_nullable = 1 THEN ' null' ELSE '' END AS type,
+					 (select object_name(fkc.referenced_object_id) from sys.foreign_key_columns fkc
+						where fkc.parent_column_id = r0.column_ordinal AND
+						fkc.parent_object_id = CAST(Object_Id(r0.source_database + '.' + r0.source_schema + '.' + r0.source_table) AS INT)) AS one_of,
+					 NULL AS array_type,
+					 Object_Id(r0.source_database + '.' + r0.source_schema + '.' + r0.source_table) AS table_id
+					 FROM sys.dm_exec_describe_first_result_set(@sourcecode, NULL, 1) AS r0
+					 UNION
+					 SELECT 
+					 (object_name(fkc.parent_object_id) + 'List') as name,
+					 NULL AS format,
+					 NULL AS sqltype,
+					 NULL AS source_column,
+					 0 AS is_nullable,
+					 1000 AS column_ordinal,
+					 'array' AS type,
+					 NULL AS one_of,
+					 object_name(fkc.parent_object_id) AS array_type,
+					 Object_Id(r1.source_database + '.' + r1.source_schema + '.' + r1.source_table) AS table_id
+					 FROM sys.dm_exec_describe_first_result_set(@sourcecode, NULL, 1) AS r1
+						INNER JOIN sys.foreign_key_columns fkc
+						ON fkc.parent_column_id = r1.column_ordinal AND
+						fkc.referenced_object_id = CAST(Object_Id(r1.source_database + '.' + r1.source_schema + '.' + r1.source_table) AS INT)) AS r
                ) AS f
                LEFT OUTER JOIN sys.extended_properties AS EP -- to get the extended properties
                  ON EP.major_id = f.table_id
@@ -83,17 +108,17 @@ DECLARE @required NVARCHAR(max), @NoColumns INT, @properties NVARCHAR(max);
                 Replace(
                  Replace(
                    Replace('{
-	  "$id": "https://mml.uk/jsonSchema/<-schema->-<-table->.json",
-	  "$schema": "http://json-schema.org/draft-07/schema#",
-	  "title": "<-table->",
-	  "SQLtablename":"'+quotename(@schema)+'.'+quotename(@table)+'",
-	  "SQLschema":"<-schema->",
-	  "type": "object",
-	  "required": [<-Required->],
-	  "maxProperties": <-MaxColumns->,
-	  "minProperties": <-MinColumns->,
-	  "properties":{'+@properties+'}
-   }', '<-minColumns->', Convert(VARCHAR(5),@NoColumns) COLLATE DATABASE_DEFAULT
+						  "$id": "https://mml.uk/jsonSchema/<-schema->-<-table->.json",
+						  "$schema": "http://json-schema.org/draft-07/schema#",
+						  "title": "<-table->",
+						  "SQLtablename":"'+quotename(@schema)+'.'+quotename(@table)+'",
+						  "SQLschema":"<-schema->",
+						  "type": "object",
+						  "required": [<-Required->],
+						  "maxProperties": <-MaxColumns->,
+						  "minProperties": <-MinColumns->,
+						  "properties":{'+@properties+'}
+					   }', '<-minColumns->', Convert(VARCHAR(5),@NoColumns) COLLATE DATABASE_DEFAULT
            	         ) , '<-maxColumns->',Convert(VARCHAR(5),@NoColumns +1) COLLATE DATABASE_DEFAULT
            	         ) , '<-Required->',@required COLLATE DATABASE_DEFAULT
            		   ) ,'<-schema->',@Schema COLLATE DATABASE_DEFAULT
